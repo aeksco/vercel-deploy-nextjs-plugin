2cff91f74185a025ee8a06aa82050c4d9b1afece
Alexander Schwartzberg <aeksco@gmail.com>
2021-03-20 20:51:47 -0400

    Updated deploy script to build nextjs app

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
-# git clone https://github.com/aeksco/vercel-deploy-nextjs-base
-# cp -R ../dist ./
-# cd vercel-deploy-nextjs-base
+git clone https://github.com/aeksco/vercel-deploy--nextjs-base ./next-app-repo
+cp -rT ./next-app-repo ./next-app
+ls -la ./next-app
+yarn install
+yarn build
+mkdir ./next-app/plugin
+cp -R ./dist ./next-app/plugin
+yarn --cwd ./next-app install
+yarn --cwd ./next-app build
+ls -la ./next-app
