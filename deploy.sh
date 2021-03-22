2f82845b7022510953ee02cbe0bce4ae82fae42b
Alexander Schwartzberg <aeksco@gmail.com>
2021-03-21 23:58:29 -0400

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@ rm -rf ./next-app/node_modules
 rm -rf ./next-app/pages
 rm ./next-app/*
 cp -rT ./next-app-repo ./next-app
+rm -rf ./next-app-repo
 # ls -la ./next-app
 # yarn --cwd install
 # yarn --cwd build
 ls -la
+ls -la ./next-app
 # mv ./dist ./next-app/plugin/dist
 # yarn --cwd ./next-app install
 # yarn --cwd ./next-app build
