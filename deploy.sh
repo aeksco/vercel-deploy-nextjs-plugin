3a200ed65f70437319a19e8c7345688c08947a79
Alexander Schwartzberg <aeksco@gmail.com>
2021-03-21 23:52:36 -0400

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 git clone https://github.com/aeksco/vercel-deploy-nextjs-base.git ./next-app-repo
 rm ./next-app/package.json
 cp -rT ./next-app-repo ./next-app
-ls -la ./next-app
-mv ./dist ./next-app/plugin/dist
-yarn --cwd ./next-app install
-yarn --cwd ./next-app build
-ls -la ./next-app
+# ls -la ./next-app
+yarn --cwd install
+yarn --cwd build
+ls -la
+# mv ./dist ./next-app/plugin/dist
+# yarn --cwd ./next-app install
+# yarn --cwd ./next-app build
+# ls -la ./next-app
